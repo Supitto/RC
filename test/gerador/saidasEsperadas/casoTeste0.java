4a0e825208943b92606b658f16fd2f53b77147c8
GabrielTrev <micrumba@yahoo.com.br>
2017-12-03 13:51:33 -0200

    Adicionada pasta contendo casos de teste do gerador

---
+++ test/gerador/saidasEsperadas/casoTeste0.java
@@ -0,0 +1,16 @@
+import robocode.*;
+
+public class misterRobot extends Robot {
+
+	public void init() {
+	}
+
+	public void loop() {
+	}
+
+	public void run() {
+		init();
+		while(true) { loop(); }
+	}
+
+}
---
+++ test/gerador/saidasEsperadas/casoTeste3.java
@@ -0,0 +1,26 @@
+import robocode.*;
+
+public class misterRobot extends Robot {
+
+	public void printNum(int num) {
+		out.println(num);
+	}
+
+	public void init() {
+		out.println("Hello, World");
+		printNum(42);
+	}
+
+	public void loop() {
+		ahead(100);
+		turnGunRight(360);
+		back(100);
+		turnGunRight(360);
+	}
+
+	public void run() {
+		init();
+		while(true) { loop(); }
+	}
+
+}
